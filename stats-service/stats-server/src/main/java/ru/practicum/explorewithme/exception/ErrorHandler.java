5b2fd7666a5341985a48c6b4e8fd871ae8d57db0
GibbedHead <chaplygin-ma@yandex.ru>
2023-07-25 00:11:10 +0300

    feat: Добавление и просмотр статистики

---
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/exception/ErrorHandler.java
@@ -0,0 +1,29 @@
+package ru.practicum.explorewithme.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice("ru.practicum.explorewithme")
+public class ErrorHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, Map<String, String>> getErrorsMap(Map<String, String> errors) {
+        Map<String, Map<String, String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+}
---
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/mapper/StatisticsMapper.java
@@ -0,0 +1,10 @@
+package ru.practicum.explorewithme.mapper;
+
+import org.mapstruct.Mapper;
+import ru.practicum.explorewithme.dto.AddHitDto;
+import ru.practicum.explorewithme.model.Statistics;
+
+@Mapper(componentModel = "spring")
+public interface StatisticsMapper {
+    Statistics AddHitToStatistics(AddHitDto addHitDto);
+}
