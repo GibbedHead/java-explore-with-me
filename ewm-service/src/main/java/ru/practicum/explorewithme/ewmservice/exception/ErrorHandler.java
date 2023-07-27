a3eb468d9120014078b22925f0df4cee030208a3
GibbedHead <chaplygin-ma@yandex.ru>
2023-07-28 01:23:59 +0300

    feat: Добавил в мейн сервис обработку исключений с выводом инфы как в сваггере fix: правильно указал установку в докер имаджах таймзоны

--- ewm-service/src/main/java/ru/practicum/explorewithme/ewmservice/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/ewmservice/exception/ErrorHandler.java
@@ -1,29 +1,48 @@
 package ru.practicum.explorewithme.ewmservice.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.explorewithme.ewmservice.exception.model.ExceptionResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 @RestControllerAdvice("ru.practicum.explorewithme")
 public class ErrorHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = ex.getBindingResult().getFieldErrors()
-                .stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-    }
-
-    private Map<String, Map<String, String>> getErrorsMap(Map<String, String> errors) {
-        Map<String, Map<String, String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ExceptionResponseEntity handleValidationErrors(MethodArgumentNotValidException ex) {
+        StringJoiner message = new StringJoiner(". ");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            StringJoiner fieldMessage = new StringJoiner("; ");
+            fieldMessage.add("Field: ".concat(fieldError.getField()));
+            fieldMessage.add("Error: ".concat(
+                            Objects.requireNonNull(
+                                    fieldError.getDefaultMessage(),
+                                    "validation error"
+                            )
+                    )
+            );
+            fieldMessage.add("Value: `".concat(
+                            Objects.requireNonNull(
+                                            fieldError.getRejectedValue(),
+                                            "default value"
+                                    ).toString()
+                                    .concat("`")
+                    )
+            );
+            message.add(fieldMessage.toString());
+        }
+        return new ExceptionResponseEntity(
+                HttpStatus.BAD_REQUEST.toString(),
+                "Incorrectly made request.",
+                message.toString(),
+                LocalDateTime.now()
+        );
     }
 }
--- ewm-service/src/main/java/ru/practicum/explorewithme/ewmservice/exception/model/ExceptionResponseEntity.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/ewmservice/exception/model/ExceptionResponseEntity.java
@@ -3,10 +3,14 @@ package ru.practicum.explorewithme.ewmservice.exception.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ExceptionResponseEntity {
